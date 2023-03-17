e89a440a03c6cfdbcc517a1eb329356e713cb3ab
Muhammad Yahaya <101328329+Muhammad235@users.noreply.github.com>
2023-03-17 05:53:29 -0700

    conical frustum surface

--- app.js
+++ app.js
@@ -110,8 +110,6 @@ function cubeSurface() {
 }
 
 
-
-
 function CylindricalTankSurface() {
     selectElement1 = document.querySelector('#cone-measure1');
     selectElement2 = document.querySelector('#cone-measure2');
@@ -179,6 +177,105 @@ function RecTankSurface() {
 
     document.querySelector('.Tank-param').textContent = param_type;
 
+}
 
+
+
+function CapsuleSurfaceArea() {
+    selectElement1 = document.querySelector('#capsule-measure1');
+    selectElement2 = document.querySelector('#capsule-measure2');
+
+    param1 = selectElement1.value;
+
+    param2 = selectElement2.value;
+
+    Baseradius = document.getElementById('capsule_radius').value;
+
+    Height = document.getElementById('capsule_height').value;
+
+    // Total surface area
+    TopSurfaceArea = `${ 2 * 3.14 * Baseradius * Baseradius}`;
+
+    document.querySelector('.CapsuleTopSA').textContent = TopSurfaceArea;
+    
+    document.querySelector('.CapsuleTopParam').textContent = param1;
+
+
+    //base surface area
+    CapsuleBottomSA = `${ 2 * 3.14 * Baseradius * Baseradius}`;
+
+    document.querySelector('.CapsuleBottomSA').textContent = CapsuleBottomSA;
+    
+    document.querySelector('.CapsuleBottom-param').textContent = param1;
+
+
+    // Lateral surface area
+
+    CapsuleLateralSA = `${2 * 3.14 * Baseradius * Height}`;
+
+    document.querySelector('.CapsuleLateralSA').textContent = CapsuleLateralSA;
+
+    document.querySelector('.Capsule_Lateral_param').textContent = param2;
+
+    // Total surface area
+    CTS = Number(TopSurfaceArea) + Number(CapsuleBottomSA) + Number(CapsuleLateralSA);
+
+    document.querySelector('.CapsuleTotalSA').textContent = CTS;
+    
+    document.querySelector('.CapsuleTotal-param').textContent = param1;
+
+}
+
+
+
+function conicalSurfaceArea() {
+    selectElement1 = document.querySelector('#conical_measure1');
+    selectElement2 = document.querySelector('#conical_measure2');
+
+    param1 = selectElement1.value;
+
+    param2 = selectElement2.value;
+
+    Topradius = document.getElementById('conicalTop_radius').value;
+
+    bottomRadius = document.getElementById('conicalBottom_radius').value;
+
+    Height = document.getElementById('conical_height').value;
+
+    // Total surface area
+
+    TotalSurfaceArea = `${3.14 * Topradius * Topradius}`;
+
+    document.querySelector('.ConicalTopSA').textContent = TotalSurfaceArea;
+    
+    document.querySelector('.ConicalTopParam').textContent = param1;
+
+
+
+    // base surface area
+    BottomSurfaceArea = `${ 3.14 * bottomRadius * bottomRadius}`;
+
+    document.querySelector('.ConicalBottomSA').textContent = BottomSurfaceArea;
+    
+    document.querySelector('.ConicalBottom_param').textContent = param2;
+
+
+    // Lateral surface area
+
+    LateralSurfaceArea = `${3.14 * (Number(bottomRadius) + Number(Topradius)) * 
+        Math.sqrt((Number(bottomRadius) - Number(Topradius)) **2 + Number(Height) **2)}`;
+
+    document.querySelector('.ConicalLateralSA').textContent = LateralSurfaceArea;
+
+    document.querySelector('.Conical_Lateral_param').textContent = param2;
+
+
+    // Total surface area
+
+    TotalSurfaceArea = Number(TotalSurfaceArea) + Number(BottomSurfaceArea) + Number(LateralSurfaceArea);
+
+    document.querySelector('.ConicalTotalSA').textContent = TotalSurfaceArea;
+
+    document.querySelector('.ConicalTotal_param').textContent = param2;
 
 }
